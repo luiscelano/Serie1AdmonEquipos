e41a097b763c430a7f8c207935ed90a4ff971bdf
Luis Celano <luiscelano@twowheelstogo.com>
2021-06-05 12:48:06 -0600

    Agregar archivos de proyecto.

---
+++ Serie1AdmonEquipos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serie1AdmonEquipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
