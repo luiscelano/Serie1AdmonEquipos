da36d064efce051070f69fbbccb2a374b60cb0b3
Luis Celano <luiscelano@twowheelstogo.com>
2021-06-05 12:51:52 -0600

    first commit

--- Serie1AdmonEquipos/Program.cs
+++ Serie1AdmonEquipos/Program.cs
@@ -6,7 +6,7 @@ namespace Serie1AdmonEquipos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! a");
         }
     }
 }
